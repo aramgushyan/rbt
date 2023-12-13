40eb0c7c4e7a549dba1b1dc2af583333db1557f6
Aram Gushyan <149182384+aramgushyan@users.noreply.github.com>
2023-12-14 00:18:44 +0500

    Add files via upload

---
+++ программа рб т.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int N, p, M;
+	do
+	{
+		cin >> N;
+	} while (N <= 3 || N % 2 == 0);
+	p = (N + 1) / 2;
+	M = (N - 1) / 2;
+	for (int i = 1; i <= p; ++i)
+	{
+		for (int j = 1; j <= M; ++j)
+		{
+			cout << ' ';
+		}
+		M = M - 1;
+		for (int k = 1; k <= 2 * i - 1; ++k)
+			cout << '*';
+		cout << endl;
+	}
+	return 0;
+}
